4f4e4b2a9e8a8b1f277952f69abd8a25e0a1852d
toZen <dpaskovskis@gmail.com>
2017-07-13 18:54:35 +0100

    Update .xmobarrc.hs

--- .xmobarrc.hs
+++ .xmobarrc.hs
@@ -1,55 +1,54 @@
 Config { 
         -- appearance
-                                 font             =   "xft:TerminusRe33:size=10:normal:antialias=true"
-                               , additionalFonts  =   [
-                                                        "xft:Terminus Re33:size=10:antialise=true:hinting=true"
-                                                       ,"xft:Terminus Re33:pixelsize=4"
-                                                       ,"xft:FontAwesome:pixelsize=12"
-                                                       ,"xft:Weather Icons:weight=bold:pixelsize=14"
-                                                       ,"xft:Terminus Re33:size=8"
-                                                      ]
-                               , iconRoot         =   "/home/tozen/.xmonad/.icons"
-                               , bgColor          =   "#1c1c1c"
-                               , fgColor          =   "#c0c0c0"
-                               , borderColor      =   "#646464"
-                               , border           =   BottomB
-                               , pickBroadest     =   False
-                               , hideOnStart      =   False
-                               , lowerOnStart     =   True
-                               , allDesktops      =   True
-                               , overrideRedirect =   True
-                               , persistent       =   True
-                               , position         =   Top
-                               , alpha            =   200
-                               , textOffset       =   12
-                               , iconOffset       =   7
-                                 commands         = 
-        -- network activity monitor
-        [ Run DynNetwork       [ "--template"     ,   "<icon=uparrow7.xbm/> <tx>kB/s <icon=downarrow7.xbm/> <rx>kB/s"
-                               , "--Low"          ,   "1000"       
-                               , "--High"         ,   "5000"       
-                               , "--low"          ,   "#d2d4dc"
-                               , "--normal"       ,   "#d2d4dc"
-                               , "--high"         ,   "#d2d4dc"
-                               ] 10
+        font             =   "xft:TerminusRe33:size=10:normal:antialias=true"
+      , additionalFonts  =  ["xft:Terminus Re33:size=10:antialise=true:hinting=true"
+                            ,"xft:Terminus Re33:pixelsize=4"
+                            ,"xft:FontAwesome:pixelsize=12"
+                            ,"xft:Weather Icons:weight=bold:pixelsize=14"
+                            ,"xft:Terminus Re33:size=8"]
+      , iconRoot         =   "/home/tozen/.xmonad/.icons"
+      , bgColor          =   "#1c1c1c"
+      , fgColor          =   "#c0c0c0"
+      , borderColor      =   "#646464"
+      , border           =   BottomB
+      , pickBroadest     =   False
+      , hideOnStart      =   False
+      , lowerOnStart     =   True
+      , allDesktops      =   True
+      , overrideRedirect =   True
+      , persistent       =   True
+      , position         =   Top
+      , alpha            =   200
+      , textOffset       =   12
+      , iconOffset       =   7
+        commands         = 
+	
+      -- network activity monitor
+      [ Run DynNetwork       [ "--template"     ,   "<icon=uparrow7.xbm/> <tx>kB/s <icon=downarrow7.xbm/> <rx>kB/s"
+                             , "--Low"          ,   "1000"       
+                             , "--High"         ,   "5000"       
+                             , "--low"          ,   "#d2d4dc"
+                             , "--normal"       ,   "#d2d4dc"
+                             , "--high"         ,   "#d2d4dc"
+                             ] 10
 
-        -- cpu activity monitor
-        , Run MultiCpu         [ "--template"     ,   "<icon=cpu1.xbm/><fc=#d2d4dc> </fc><total0>%"
-                               , "--Low"          ,   "50"         
-                               , "--High"         ,   "85"
-                               , "--low"          ,   "#d2d4dc"
-                               , "--normal"       ,   "#d2d4dc"
-                               , "--high"         ,   "#fd0537"
-                               ] 10
+      -- cpu activity monitor
+      , Run MultiCpu         [ "--template"     ,   "<icon=cpu1.xbm/><fc=#d2d4dc> </fc><total0>%"
+                             , "--Low"          ,   "50"         
+                             , "--High"         ,   "85"
+                             , "--low"          ,   "#d2d4dc"
+                             , "--normal"       ,   "#d2d4dc"
+                             , "--high"         ,   "#fd0537"
+                             ] 10
 
-        -- cpu core temperature monitor
-        , Run CoreTemp         [ "--template"     ,   "<icon=temp1.xbm/><fc=#d2d4dc> </fc><core0>°C"
-                               , "--Low"          ,   "2"         
-                               , "--High"         ,   "80"        
-			       , "--low"          ,   "#d2d4dc"
-                               , "--normal"       ,   "#d2d4dc"
-                               , "--high"         ,   "fd0537"
-                               ] 50
+      -- cpu core temperature monitor
+      , Run CoreTemp         [ "--template"     ,   "<icon=temp1.xbm/><fc=#d2d4dc> </fc><core0>°C"
+                             , "--Low"          ,   "2"         
+                             , "--High"         ,   "80"        
+			     , "--low"          ,   "#d2d4dc"
+                             , "--normal"       ,   "#d2d4dc"
+                             , "--high"         ,   "fd0537"
+                             ] 50
                           
         -- memory usage monitor
         , Run Memory           [ "--template"     ,   "<icon=mem1.xbm/><fc=#d2d4dc> </fc><usedratio>%"
@@ -108,9 +107,9 @@ Config {
         , Run Com              "XMTime"           []  ""            10
         ]
         -- layout
-   , sepChar  =  "%"   -- delineator between plugin names and straight text
-   , alignSep =  "}{"  -- separator between left-right alignment
-   , template =  " <fc=#3aa4db>|</fc> %XMonadLog% <fc=#3aa4db>|</fc> %coretemp% <fc=#3aa4db>|</fc><action=`XMFree` button=1> %memory% </action><fc=#3aa4db>|</fc><action=`XMVnstat` button=1> %XMTraf% </action><fc=#3aa4db>|</fc><action=`XMTop-cpu` button=1> %multicpu% %TCPU%</action><fc=#3aa4db>|</fc>}<fc=#3aa4db>|</fc><action=`XMCal` button=1> %XMTime% </action><fc=#3aa4db>|</fc> {<action=`XMYaourt` button=1>%Update%%XMUpdate%</action> <fc=#3aa4db>|</fc> <icon=cpu5.xbm/> %uname% <fc=#3aa4db>|</fc> <fc=#c0c0c0><icon=vol2.xbm/> %myVolume% </fc><fc=#3aa4db>|</fc> <action=`XMncmpcpp` button=3><action=`mpc prev` button=1><fn=3></fn></action> <action=`mpc toggle` button=1>%XMMPD%</action> <action=`mpc next` button=1><fn=3></fn></action></action> <fc=#3aa4db>|</fc> %wlp2s0wi% <fc=#3aa4db>|</fc> %battery% <fc=#3aa4db>|</fc> "
+        , sepChar  =  "%"
+        , alignSep =  "}{"
+        , template =  " <fc=#3aa4db>|</fc> %XMonadLog% <fc=#3aa4db>|</fc> %coretemp% <fc=#3aa4db>|</fc><action=`XMFree` button=1> %memory% </action><fc=#3aa4db>|</fc><action=`XMVnstat` button=1> %XMTraf% </action><fc=#3aa4db>|</fc><action=`XMTop-cpu` button=1> %multicpu% %TCPU%</action><fc=#3aa4db>|</fc>}<fc=#3aa4db>|</fc><action=`XMCal` button=1> %XMTime% </action><fc=#3aa4db>|</fc> {<action=`XMYaourt` button=1>%Update%%XMUpdate%</action> <fc=#3aa4db>|</fc> <icon=cpu5.xbm/> %uname% <fc=#3aa4db>|</fc> <fc=#c0c0c0><icon=vol2.xbm/> %myVolume% </fc><fc=#3aa4db>|</fc> <action=`XMncmpcpp` button=3><action=`mpc prev` button=1><fn=3></fn></action> <action=`mpc toggle` button=1>%XMMPD%</action> <action=`mpc next` button=1><fn=3></fn></action></action> <fc=#3aa4db>|</fc> %wlp2s0wi% <fc=#3aa4db>|</fc> %battery% <fc=#3aa4db>|</fc> "
       
    }
 
